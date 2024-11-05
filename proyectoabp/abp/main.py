0aaaa66429390fd76dc544b381c36a536b81af30
santiagoXIII <vazquezbasilioangelsantiago@cetis50cdmx.com>
2024-11-05 11:05:39 -0600

    Add files via upload

--- proyectoabp/abp/main.py
+++ proyectoabp/abp/main.py
@@ -43,6 +43,40 @@ def main(page: ft.Page):
                     bgcolor=page.bgcolor
                 )
             )
+        #vista que es stem
+        if page.route == "/STEAM":
+            page.views.append(
+                View(
+                    "/STEAM",
+                    controls=[
+                        AppBar(
+                            title=ft.Text("¿Que es STEM?",size=15),
+                            bgcolor="gray"
+                        ),
+                        ft.Container(
+                            ft.Row(
+                                controls=[
+                                    ft.Column(
+                                            controls=[
+                                                ft.Text("¿Que es STEM?",size=30),
+                                                ft.Text("STEM es el acrónimo en inglés que hace referencia a Science, Technology, Engineering and Mathematics (ciencia, tecnología, ingeniería y matemáticas), y que plantea la integración interdisciplinaria de estas áreas de las ciencias en un contexto asociado a la ingeniería y la tecnología. ", size=15, width=700,height=100,text_align="JUSTIFY"),
+                                                ft.Row(controls=[
+                                                    ft.ElevatedButton("ciencia"),
+                                                    ft.ElevatedButton("tecnologia"),
+                                                    ft.ElevatedButton("ingenieria"),
+                                                    ft.ElevatedButton("matematicas")]),
+                                                ft.ElevatedButton("volver al menu",on_click= lambda _: page.go("/"))
+                                            ],alignment=MainAxisAlignment.CENTER
+                                    )
+                                        ],alignment=MainAxisAlignment.CENTER
+                                    ),
+                            bgcolor=page.bgcolor,
+                            expand=True
+                        )
+                    ],
+                    bgcolor=page.bgcolor
+                )
+            )
         page.update()
 
     page.on_route_change = route_change
